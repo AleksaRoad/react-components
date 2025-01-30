f71692a7d5a0968c131cecbada46107764a6e0e0
AleksaRoad <aleksaroad@gmail.com>
2025-01-30 18:24:10 +0300

    fix: change input type from text to search

--- src/SearchForm/SearchForm.tsx
+++ src/SearchForm/SearchForm.tsx
@@ -3,7 +3,7 @@ import styles from './SearchForm.module.css';
 export const SearchForm = () => {
   return (
     <>
-      <input className={styles.input} type="text" />
+      <input className={styles.input} type="search" />
       <button className={styles.button} type="submit">
         Search
       </button>
