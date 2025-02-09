f838f73c89c3aac279b96aaf6da0ac97d8ecd1fd
AleksaRoad <aleksaroad@gmail.com>
2025-02-09 04:21:44 +0300

    fix: remove MOCK_DATA in index.ts exports

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,8 +1,3 @@
-export {
-  CACHE_KEY,
-  ERROR_MESSAGES,
-  MOCK_CHARACTERS_DATA,
-  PAGE_SIZE,
-} from './constants';
+export { CACHE_KEY, ERROR_MESSAGES, PAGE_SIZE } from './constants';
 export { useLoadData, useStorage } from './hooks';
 export type { RickAndMortyCharacter } from './types';
