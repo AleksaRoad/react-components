3e4f3b8860f68afec68b9d5c6487ff068a245a9a
AleksaRoad <aleksaroad@gmail.com>
2025-02-09 18:32:50 +0300

    fix: updated components passed inside index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { ErrorDisplay } from './ErrorDisplay';
 export { Footer, PaginationControl } from './Footer';
-export { Header } from './Header';
+export { Header, SearchForm } from './Header';
 export { HomePage } from './HomePage';
 export { CharacterPage, Main } from './Main';
 export { NotFound } from './NotFound';
